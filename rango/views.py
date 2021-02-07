4529d77492200c1ab2f39f21245dda4fc4fb8b70
“Paul <2139206e@student.gla.ac.uk>
2021-02-07 19:45:00 +0000

    chapter 3 fixed full

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-	return render(request, 'rango/index.html', context=context_dict)
+	#context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	#return render(request, 'rango/index.html', context=context_dict)
 	return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 	
 def about(request):
